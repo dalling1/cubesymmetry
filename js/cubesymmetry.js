2c83c6c1a0bff9a12cb87a3b2cfce328c96df1e6
David Allingham <David.Allingham@newcastle.edu.au>
2020-06-25 11:09:21 +1000

    Added basic HTML and Javascript files, based on Free Product of Graphs repo https://github.com/dalling1/freeproductgraphs

---
+++ js/cubesymmetry.js
@@ -0,0 +1,2 @@
+document.getElementById("graphareaCubeGraph").setAttribute("style","width:"+document.getElementById("cubeGraphControls").clientWidth+"px;");
+document.getElementById("thecubegraph").setAttribute("style","width:"+document.getElementById("cubeGraphControls").clientWidth+"px;");
